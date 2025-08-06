ffa60e6ebfad118110e484901eacd3e3f2470ec3
Jesús Méndez Quesada <45716413+JesusChuz@users.noreply.github.com>
2025-08-06 16:40:46 -0600

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -22,7 +22,7 @@ def run(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully. Build with ubuntu 22-04")
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully. Build with ubuntu 22-04 and bash task")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
